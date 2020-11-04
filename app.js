3d98cbc787e60e4278d2f0954f5801f109a16623
Olusola i.t <Holushawlar.it@gmial.com>
2020-11-04 09:26:35 -0600

    finished

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function changeBodyBg(color){
     document.getElementById("side2").style.backgroundColor = color;
 }
+
+function changeBtn(color){
+    document.getElementById("subBtn").style.backgroundColor = color;
+}
